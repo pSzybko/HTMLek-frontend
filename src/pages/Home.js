6eeab761877591d2cd7a75a2b8bc870d0e441205
pszybko <pozdro.benc@gmail.com>
2022-12-15 23:16:03 +0100

    Home styling changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,16 +6,16 @@ import './Home.css'
 
 export default function Home() {
     return (
-        <>
+        <div className='home' id='home'>
             <div className='animationWrapper'>
                 <Animation modelPath="/Home.stl" />
             </div>
-            <div className='home' id='home' >
+            <div   >
                 <div className='Text-box'>
                     <h1 className='Title'>&lt;<strong>p</strong>&gt;HTMLek&lt;/<strong>p</strong>&gt;</h1>
                     <h3 className='Subtitle'>System edukacyjny do nauki języka HTML/CSS na poziomie szkolnym</h3>
                 </div>
             </div >
-        </>
+        </div>
     )
 }
